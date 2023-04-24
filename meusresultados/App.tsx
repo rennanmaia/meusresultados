a8660b404ac56338bc5d0ca27b5dfd595debf59a
Rennan Maia <rennanmaia@gmail.com>
2023-04-24 16:29:14 -0300

    style: styling with StyleSheet

--- meusresultados/App.tsx
+++ meusresultados/App.tsx
@@ -1,28 +1,34 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#d9d9d9',
-      padding: 12,
-    }}>
-      <Text style={{
-        width: '100%',
-        textAlign: 'center',
-        fontWeight: 'bold',
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.texto1}>
         Primeiro texto
       </Text>
-      <Text style={{
-        width: '100%',
-        height: 100,
-        backgroundColor: '#804000',
-        color: '#ffffff',
-      }}>
+      <Text style={styles.texto2}>
         Segundo texto
       </Text>
       <Text>Terceiro texto</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d9d9d9',
+    padding: 12,
+  },
+  texto1: {
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  texto2 : {
+    width: '100%',
+    height: 100,
+    backgroundColor: '#804000',
+    color: '#ffffff',
+  }
+})
