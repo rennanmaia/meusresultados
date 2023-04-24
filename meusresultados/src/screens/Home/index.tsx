c29722045ae47e5b67838850c53de9eeeac296b5
Rennan Maia <rennanmaia@gmail.com>
2023-04-24 16:40:35 -0300

    chore: removing export default from Home

--- meusresultados/src/screens/Home/index.tsx
+++ meusresultados/src/screens/Home/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto1}>
