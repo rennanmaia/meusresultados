775370915874202dd50f8cdd8669bad70f06255b
Rennan Maia <rennanmaia@gmail.com>
2023-04-24 16:21:21 -0300

    style: simple styling

--- meusresultados/App.tsx
+++ meusresultados/App.tsx
@@ -2,10 +2,27 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <Text>Primeiro texto</Text>
-      <Text>Segundo texto</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#d9d9d9',
+      padding: 12,
+    }}>
+      <Text style={{
+        width: '100%',
+        textAlign: 'center',
+        fontWeight: 'bold',
+      }}>
+        Primeiro texto
+      </Text>
+      <Text style={{
+        width: '100%',
+        height: 100,
+        backgroundColor: '#804000',
+        color: '#ffffff',
+      }}>
+        Segundo texto
+      </Text>
       <Text>Terceiro texto</Text>
-    </>
+    </View>
   );
 }
